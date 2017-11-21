a3d871e9795efe390ac7e21d7d428035c2b22c7a
JhamniYoungShinnick <jyou1904@mpsedu.org>
2017-11-21 09:21:54 -0600

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,15 @@
+
+  // the setup function runs oncewhen you press reset or power on the board
+void setup() 
+{// initialize digital pin LED_BUILTIN as an out put
+pinMode(10, OUTPUT);
+pinMode(5, INPUT);
+pinMode(11, OUTPUT);
+pinMode(4, INPUT);
+
+}
+
+void loop() {
+digitalWrite(11, digitalRead(5));
+digitalWrite(10, digitalRead(4));
+}
---
+++ Dimmer.ino
@@ -0,0 +1,37 @@
+//Variables for LEDs
+#define RED 10 //# for arduino port
+#define GREEN 11
+//Variables for Buttons
+#define B1 4
+#define B2 5
+void setup() {
+  // put your setup code here, to run once:
+//Light is a type of output
+pinMode(RED, OUTPUT);
+pinMode(GREEN, OUTPUT);
+Serial.begin(9600);
+//Buttons require input
+pinMode(B1, INPUT);
+pinMode(B2, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int joystick;
+int red, green;
+joystick = analogRead(A0);
+if (joystick > 512) {
+  green =map(joystick, 512, 1024, 0, 255);
+  red=0;
+}
+else {
+  red = map(joystick, 0, 510, 255, 0);
+  red = -1 * red;
+  green = 0;
+}
+analogWrite(RED, red);
+analogWrite(GREEN, green);
+Serial.println(joystick); 
+digitalWrite(11, digitalRead(5));
+digitalWrite(10, digitalRead(4)); 
+}
